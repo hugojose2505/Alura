5591b7f3448d2ff71591dc3c850eb89f3e0085bb
HUgo <hugo.jose.terra@hotmail.com>
2023-04-18 20:55:56 -0300

    Pagina Alura Play finalizado

---
+++ Alura Play/js/buscarVideo.js
@@ -0,0 +1,24 @@
+import { conectAPI } from "./conectAPI.js";
+import constroiCard from "./mostrarVideos.js"
+
+async function buscarVideo(evento){
+    evento.preventDefault()
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value
+
+    const busca = await conectAPI.buscaVideo(dadosDePesquisa)
+
+    const lista = document.querySelector("[data-lista")
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild)
+    }
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo,elemento.descricao,elemento.url,elemento.imagem)))
+
+    if(busca.lenght == 0){
+        lista.innerHTML = `<h2 class="mensagem__titulo"> Nao existem videos com esse termo" </h2>`
+    }
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisar]")
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
